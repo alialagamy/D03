31f76555073ffe44d4e636c49cce6061af284d70
dell <dell@ENGALI>
2025-01-23 14:49:55 +0300

    Add project files.

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Arithmetic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* chalnge 1
+             bool wearCoat = false;
+             double temperature = 0;
+             int windSpeed = 0;
+
+             const int windSpeedForCoat = 25;
+             const double temperatureForCoat = 22;
+
+             Console.WriteLine("Enter temperture: ");
+             temperature = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine("Enter windspeed: ");
+             windSpeed = Convert.ToInt32(Console.ReadLine());
+
+             wearCoat = (windSpeed > windSpeedForCoat && temperature < temperatureForCoat);
+             Console.WriteLine(wearCoat);*/
+
+            /* chalnge 2
+             Console.WriteLine("hello");
+            Console.WriteLine("Please enter length");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter width");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter higth");
+            int higth = Convert.ToInt32(Console.ReadLine());
+
+            double size = length *  width * higth;
+            double service = 2.5;
+
+            if (size > 100 && size < 500)
+            {
+                Console.WriteLine($"total = {service * 1.1:c}");
+            }
+            else if (size > 500)
+            {
+                Console.WriteLine($"total = {service * 1.25:c}");
+            } else 
+            {
+                Console.WriteLine($"total = {service:c}"); */
+
+            /* chalnge 3
+            int code = 0;
+            Console.WriteLine( "Enter code ");
+             code = Convert.ToInt32(Console.ReadLine());
+            switch (code)
+            {
+                case 1:
+                    Console.WriteLine("satr");
+                    break;
+                case 2:
+                    Console.WriteLine("sun");
+                    break;
+                case 3:
+                    Console.WriteLine("mon");
+                    break;
+                case 4:
+                    Console.WriteLine("the");
+                    break;
+                case 5:
+                    Console.WriteLine("wed");
+                    break;
+                case 6:
+                    Console.WriteLine("thu");
+                    break;
+                case 7:
+                    Console.WriteLine("fri");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a value between 1 and 7");
+                    break;
+            }*/
+
+        }
+
+    }
+}
+    
+
---
+++ Task/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char Selection = '\0';
+            List<int> Inputs = new List<int>();
+            
+            do
+            {
+                Console.WriteLine("---------------------- \n Main manu");
+                Console.WriteLine("\n P - Print numbers" +
+                    " \n A - Add a number" +
+                    " \n M - Display mean of the numbers" +
+                    " \n S - Display the smallest number" +
+                    " \n L - Display the largest number"+
+                    " \n C - Clear list"+
+                    " \n Q - Quit");
+                Console.WriteLine("Enter your selection: ");
+                Selection = char.ToUpper(char.Parse(Console.ReadLine()));
+
+                switch (Selection)
+                {
+                    case 'P':
+                        {
+                            if (Inputs.Any())
+                            {
+                                Console.WriteLine($"[ {string.Join(" ", Inputs)} ]");
+                            }
+                            else
+                            {
+                                Console.WriteLine("List is empty");
+                            }
+                            break;
+                        }
+
+                    case 'A':
+                        {
+                            int number = 0;
+                            Console.WriteLine("Enter number :");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            Inputs.Add(number);
+                            Console.WriteLine($"{number} is added");
+                            break;
+                        }
+                   
+                    case 'M': 
+                        {
+                            if (Inputs.Count > 0)
+                            {
+                                double sum = 0;
+                                for (int i = 0; i < Inputs.Count; i++)
+                                {
+                                    sum += Inputs[i];
+                                }
+                                double mean = sum / Inputs.Count;
+                                Console.WriteLine($"The mean of numbers : {mean}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("List empty");
+                            }
+                            break;
+                        }
+                   
+                    case 'S':
+                        {
+                            if ( Inputs.Count > 0 )
+                            {
+                                int smallest = Inputs[0];
+                                for (int i = 1; i < Inputs.Count; i++)
+                                {
+                                    if (Inputs[i] < smallest)
+                                    {
+                                        smallest = Inputs[i];
+                                    }
+                                }
+                                Console.WriteLine("the smallest number : " + smallest);
+                            }
+                            else
+                            {
+                                Console.WriteLine("list empty");
+                            }
+                            break;
+                        }
+                   
+                    case 'L':
+                        {
+                            if (Inputs.Count > 0)
+                            {
+                                int largest = Inputs[0];
+                                for (int i = 1; i < Inputs.Count; i++)
+                                {
+                                    if (Inputs[i] > largest)
+                                    {
+                                        largest = Inputs[i];
+                                    }
+                                }
+                                Console.WriteLine("the Largest number : " + largest);
+                            }
+                            else
+                            {
+                                Console.WriteLine("list empty");
+                            }
+                            break;
+                        }
+                   
+                    case 'C':
+                        {
+                            Inputs.Clear();
+                            Console.WriteLine("List empty");
+                            break;
+                        }
+                   
+                    case 'Q':
+                        {
+                            Console.WriteLine("Good bye");
+                            break;
+                        }
+                   
+                    default:
+                        Console.WriteLine("Unknown selection");
+                        break;
+                }
+            } while (Selection != 'Q');
+        }
+    }
+}
